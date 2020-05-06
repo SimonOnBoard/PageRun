5ec9f332893e20e9a321c4aa2076f48b369f7bae
SPB13F <sweetsixteen.sudo@gmail.com>
2020-05-06 18:50:46 +0300

    runner

--- src/main/java/Multihash.java
+++ src/main/java/Multihash.java
@@ -1,8 +1,10 @@
 import com.opencsv.CSVReader;
+import starter.Pair;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.*;
 import java.util.zip.Adler32;
 import java.util.zip.CRC32;
@@ -13,7 +15,7 @@ public class Multihash {
         try (CSVReader csvReader = new CSVReader(new FileReader("transactions.csv"))) {
             String[] values = null;
             while ((values = csvReader.readNext()) != null) {
-                records.add(Arrays.asList(values));
+                records.add(Arrays.asList(values[0].split(";")));
             }
         }
         Adler32 adler32 = new Adler32();
@@ -21,7 +23,55 @@ public class Multihash {
         System.out.println(records.size());
         Map<String, Integer> countMap = new HashMap<>();
         Map<String, List<String>> bucketMap = new HashMap<>();
-        for(int i = 0; i < records.size(); i++){
+        String bucket;
+        String product;
+        for (int i = 1; i < records.size(); i++) {
+            bucket = records.get(i).get(1);
+            product = records.get(i).get(0);
+            if (countMap.get(product) != null) {
+                countMap.put(product, countMap.get(product) + 1);
+            } else {
+                countMap.put(product, 0);
+            }
+
+            if (bucketMap.get(bucket) != null) {
+                List<String> b = bucketMap.get(bucket);
+                b.add(product);
+                bucketMap.put(bucket, b);
+            } else {
+                bucketMap.put(bucket, new ArrayList<>());
+            }
         }
+        System.out.println("Buckets size: " + bucketMap.keySet().size());
+        System.out.println("Product size: " + countMap.keySet().size());
+        Map<String, Integer> numberMap = new HashMap<>();
+        int i = 0;
+        for (Map.Entry<String, Integer> entry : countMap.entrySet()) {
+            numberMap.put(entry.getKey(), i);
+            i++;
+        }
+        System.out.println("Number map: " + numberMap.size());
+        System.out.println(i + 1);
+        Map<String,List<Pair<Integer,Integer>>> bucketPairs = new HashMap<>();
+        int size;
+        for(String buck: bucketMap.keySet()){
+            List<String> strings = bucketMap.get(buck);
+            System.out.println(strings);
+            size = strings.size();
+            List<Pair<Integer,Integer>> pairs = new ArrayList<>();
+            bucketPairs.put(buck, new ArrayList<>());
+            for(int s = 0; s < size - 1; s++){
+                for(int end = s; end < size; end++){
+                    String first = strings.get(s);
+                    String last = strings.get(end);
+                    pairs.add(new Pair<>(numberMap.get(first), numberMap.get(last)));
+                }
+            }
+            System.out.println();
+            System.out.println("Bucket size: " + size);
+            System.out.println("Pairs size: " + pairs.size());
+            break;
+        }
+
     }
 }
